cafabff4cc5ac7006312a7008436a2aa4d6c90ae
joshjosky <ubajoshua17@gmail.com>
2023-03-16 07:35:12 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * with proper grammar, but the outcome is a piece of art,
+ *
+ * Return 0 (Success)*/
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
