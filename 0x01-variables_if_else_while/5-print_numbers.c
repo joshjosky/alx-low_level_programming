8985755a8f6f3f32f0e3a41d0fbd45b03ff56ec6
joshjosky <ubajoshua17@gmail.com>
2023-03-19 02:41:44 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: a code that prints all single digit of base 10
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
