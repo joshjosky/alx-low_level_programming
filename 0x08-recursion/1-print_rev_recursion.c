04dfaff8214ad0e9980dc97319474703520b3a8e
joshjosky <ubajoshua17@gmail.com>
2023-04-06 03:53:05 -0700

    Print reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,15 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
-	}
-	else
-	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		s++;
-		_print_rev_recursion(s);
 	}
 }
