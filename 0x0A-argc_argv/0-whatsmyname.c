a141cfc4e813c61b4663f3670e026b080bb90474
joshjosky <ubajoshua17@gmail.com>
2023-04-10 09:42:01 -0700

    What is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the codes
+ * @argc: argument count
+ * @*argv[]: a variable that holds the arrays of strings
+ * Return: 0 Always (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
