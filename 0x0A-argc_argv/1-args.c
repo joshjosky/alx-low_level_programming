c7a8fb44ec7155c511a6b042e2ddb000b966cecf
joshjosky <ubajoshua17@gmail.com>
2023-04-10 10:50:52 -0700

    Argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the codes
+ * @argc: argument count
+ * @argv: a variable that holds the arrays of strings
+ * Return: 0 Always (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
